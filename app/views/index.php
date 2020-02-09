dbeb4d55e04ef7103493a77fb00551b7f65da861
Zvi <io.windygo.co.il>
2020-02-09 16:29:01 +0200

    Views index.php updated

--- app/views/index.php
+++ app/views/index.php
@@ -6,12 +6,7 @@
   
   <header><h1><?php echo $data['title']; ?></h1></header>  
   <div class="main p-2">    
-      <p>This is the Eshel MVC PHP framework - Please refer to the DOCS on how to use it</p>
-      <hr>
-      <ul>
-      <li>1. Edit: RewriteBase /mvc/public in .htaccess - Change mvc to your folder name</li>
-      <li>2. Edit: /app/config/conifg.php lines 2-4 to match your db user/pass/name respectively</li>
-      </ul>
+      <p>This is the Eshel MVC PHP framework - Please refer to the README on how to use it</p>    
   </div>
 </div>
 
