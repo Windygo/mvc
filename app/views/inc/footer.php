b2a72fe8e6d1b7514fb6490b1fcc9bf1596a2903
Zvi <io.windygo.co.il>
2020-02-09 16:31:54 +0200

    footer upated

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,5 +1,5 @@
 <footer>
-<p>&copy; 2020 - All right reserved - Zvi Eshel</p>
+<p>&copy; 2020 - All rights reserved - Zvi Eshel</p>
 </footer>
 <script src="<?php echo '/' . SITE_NAME . '/public/js/main.js';?>"></script>
 </body>
